f2492440c8cfedc9e7918d9c1bcc9d9b00dfb9ad
Yuri Ferraz <yurigsferraz@gmail.com>
2021-08-20 13:13:16 -0300

    Cria middleware para verificar se a conta existe

--- src/index.js
+++ src/index.js
@@ -5,13 +5,22 @@ const app = express();
 
 app.use(express.json());
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- */
 const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found!" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
@@ -34,14 +43,10 @@ app.post("/account", (request, response) => {
     return response.status(201).send();
 });
 
-app.get("/statement/", (request, response) => {
-    const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer) {
-        return response.status(400).json({ error: "Customer not found!" });
-    }
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
 });

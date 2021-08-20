131ac3668b019ad64067480d814d9ab0a0040be9
Yuri Ferraz <yurigsferraz@gmail.com>
2021-08-20 14:54:10 -0300

    Adiciona funcionalidade de sacar da conta

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function verifyIfExistsAccountCPF(request, response, next) {
 
     const customer = customers.find((customer) => customer.cpf === cpf);
 
-    if(!customer) {
+    if (!customer) {
         return response.status(400).json({ error: "Customer not found!" });
     }
 
@@ -22,13 +22,25 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount;
+        }else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
 
     const customerAlreadyExists = customers.some(
         (customer) => customer.cpf === cpf
     );
-    
+
     if (customerAlreadyExists) {
         return response.status(400).json({ error: "Customer already exists!" });
     }
@@ -54,7 +66,7 @@ app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
 app.post("/deposit/", verifyIfExistsAccountCPF, (request, response) => {
     const { customer } = request;
     const { description, amount } = request.body;
-    
+
     const statementOperation = {
         description,
         amount,
@@ -66,4 +78,26 @@ app.post("/deposit/", verifyIfExistsAccountCPF, (request, response) => {
 
     return response.status(201).send();
 });
+
+app.post("/withdraw/", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { amount } = request.body;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
+
 app.listen(3333);
